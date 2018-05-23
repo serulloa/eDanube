2b5c17373dd25af393af5a776cd557e80dedaaa6
Sergio Ulloa <sergio95rlc@gmail.com>
2018-05-23 13:01:23 +0200

    [Update] GreetingController - [Add] User

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,17 +1,19 @@
 package hello;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
 
+    @RequestMapping("/greeting")
+    public User greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new User(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
 }
