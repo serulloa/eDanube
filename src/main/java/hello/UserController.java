171c58ca75699234df5e4b457ee618bb6b25b0a2
serulloa <sergio95rlc@gmail.com>
2018-05-25 14:24:00 +0200

    Registro terminado

--- src/main/java/hello/UserController.java
+++ src/main/java/hello/UserController.java
@@ -3,6 +3,7 @@ package hello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
@@ -12,7 +13,7 @@ public class UserController {
 	private UserRepository repo;
 
     @RequestMapping("/register")
-    public String register(@RequestParam(value="name") String name,
+    public RedirectView register(@RequestParam(value="name") String name,
     					 @RequestParam(value="lastName") String lastName,
     					 @RequestParam(value="age") int age,
     					 @RequestParam(value="gender") Gender gender,
@@ -20,13 +21,10 @@ public class UserController {
     					 @RequestParam(value="password") String password,
     					 @RequestParam(value="passwordConf") String passwordConf) {
     	
-    	if (password.equals(passwordConf)) {
-    		User user = new User(name, lastName, age, gender, email, password);
-    		repo.save(user);
-    		return "ok";
-    	}
-    	else 
-    		return null;
+    	
+		User user = new User(name, lastName, age, gender, email, password);
+		repo.save(user);
+		return new RedirectView("login.html");
     }
     
 }
--- src/main/resources/static/script/main.js
+++ src/main/resources/static/script/main.js
@@ -1,10 +1,10 @@
 /**
- * 
+ *
  */
 
 function register() {
 	event.preventDefault();
-	
+
 	var ok = true;
 	var name = document.forms["registerForm"]["name"].value;
 	var lastName = document.forms["registerForm"]["lastName"].value;
@@ -13,25 +13,13 @@ function register() {
 	var email = document.forms["registerForm"]["email"].value;
 	var password = document.forms["registerForm"]["password"].value;
 	var passwordConf = document.forms["registerForm"]["passwordConf"].value;
-	
+
 	if (password != passwordConf) {
 		alert("Ambas contraseñas deben coincidir.")
-		ok = false;
+		ok = false
 	}
 	
-	if(ok) {
-		alert(name)
-		$.ajax({
-			type: "POST",
-			url: "/register",
-			data: {
-				name : name,
-				lastName : lastName,
-				age : age,
-				gender : gender,
-				email : email,
-				password : password
-			}
-		})
-	}
+	if(ok)
+		document.forms["registerForm"].submit();
+
 }
